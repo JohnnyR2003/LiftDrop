c3baecb45b306740ab51f5b074214e78538957d5
49502 - Gonçalo Morais <goncalo.p.morais@hotmail.com>
2025-03-12 16:00:21 +0000

    base spring

---
+++ code/backend/liftdrop/src/main/kotlin/isel/pt/liftdrop/LiftdropApplication.kt
@@ -0,0 +1,11 @@
+package isel.pt.liftdrop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LiftdropApplication
+
+fun main(args: Array<String>) {
+	runApplication<LiftdropApplication>(*args)
+}
---
+++ code/backend/liftdrop/src/test/kotlin/isel/pt/liftdrop/LiftdropApplicationTests.kt
@@ -0,0 +1,13 @@
+package isel.pt.liftdrop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LiftdropApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
