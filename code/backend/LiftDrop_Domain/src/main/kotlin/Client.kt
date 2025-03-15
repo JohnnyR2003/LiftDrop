56a9c60a904a6181c1ccb07c8d41729ee213e145
49424 - Joao Ramos <a49424@alunos.isel.pt>
2025-03-15 22:30:55 +0000

    *UPDATED* User and Client domain classes

--- code/backend/LiftDrop_Domain/src/main/kotlin/Client.kt
+++
@@ -1,7 +0,0 @@
-package pt.isel.pipeline
-
-class Client(
-    override val id: Long,
-    override val email: String,
-    override val password: String
-) : User()
--- code/backend/LiftDrop_Domain/src/main/kotlin/User.kt
+++ code/backend/LiftDrop_Domain/src/main/kotlin/User.kt
@@ -1,7 +1,14 @@
 package pt.isel.pipeline
 
-sealed class User {
-    abstract val id: Long
-    abstract val email: String
-    abstract val password: String
-}
+sealed class User (
+   val id: Long,
+   val email: String,
+   val password: String
+)
+
+class Client(
+    id: Long,
+    email: String,
+    password: String,
+    val name: String,
+) : User(id, email, password)
