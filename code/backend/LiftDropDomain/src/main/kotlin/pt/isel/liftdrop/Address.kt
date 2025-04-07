2f2df172a3f007ac9136ba685bcba98e491bcd66
49424 - Joao Ramos <a49424@alunos.isel.pt>
2025-04-07 12:44:19 +0100

    *ADDED* Address data class to represent location details in the LiftDrop application and also updated the current ER diagram with the Address entity

---
+++ code/backend/LiftDropDomain/src/main/kotlin/pt/isel/liftdrop/Address.kt
@@ -0,0 +1,10 @@
+package pt.isel.pipeline.pt.isel.liftdrop
+
+data class Address(
+    val id: Long,
+    val city: String,
+    val street: String,
+    val streetNumber: String,
+    val zipCode: String,
+    val country: String,
+)
