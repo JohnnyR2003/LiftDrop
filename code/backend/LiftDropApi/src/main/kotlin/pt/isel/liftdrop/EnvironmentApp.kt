2edc9efd4613474f8cf920266f11c06871287766
49502 - Gonçalo Morais <goncalo.p.morais@hotmail.com>
2025-05-29 19:44:54 +0100

    Refactor DB_URL handling for simplified parsing.
    Replaces regex-based database URL parsing with a cleaner `split` logic. Also comments out redundant environment setup in test tasks for better clarity and maintainability. These changes enhance code readability and reduce complexity.

--- code/backend/LiftDropApi/src/main/kotlin/pt/isel/liftdrop/EnvironmentApp.kt
+++ code/backend/LiftDropApi/src/main/kotlin/pt/isel/liftdrop/EnvironmentApp.kt
@@ -2,30 +2,23 @@ package pt.isel.liftdrop
 
 object EnvironmentApp {
     fun getDbUrl(): String {
-        val dbUrl = System.getenv("DB_URL")
-        return if (dbUrl == null) {
-            "jdbc:postgresql://localhost:5432/liftdrop"
-        } else {
-            "jdbc:$dbUrl"
-        }
+        val dbUrl = System.getenv("DB_URL") ?: return "jdbc:postgresql://localhost:5432/liftdrop"
+        return "jdbc:${dbUrl}"
     }
 
     fun getDbUser(): String {
         val dbUrl = System.getenv("DB_URL") ?: return "postgres"
-        val regex = Regex(".*://(.*?):.*?@")
-        return regex.find(dbUrl)?.groupValues?.get(1) ?: "postgres"
+        return dbUrl.split("://")[1].split(":")[0]
     }
 
     fun getDbPassword(): String {
         val dbUrl = System.getenv("DB_URL") ?: return "postgres"
-        val regex = Regex(".*://.*?:(.*?)@")
-        return regex.find(dbUrl)?.groupValues?.get(1) ?: "postgres"
+        return dbUrl.split(":")[2].split("@")[0]
     }
 
-    // jdbc:postgresql://localhost:5432/liftdrop?user=postgres&password=postgres
+//    postgresql://liftdrop_ceib_user:NjEbzbVyGBsvF0AoG7c8C4VOp5u3OblK@dpg-d0s9dkjuibrs738179c0-a/liftdrop_ceib
 
     fun getGoogleAPIKey(): String =
         System.getenv("GOOGLE_API_KEY")
             ?: throw IllegalStateException("GOOGLE_API_KEY environment variable not set")
 }
-//
