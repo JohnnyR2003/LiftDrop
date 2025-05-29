a5492ff8cd388ea403056231dead0f925a46f956
49502 - Gonçalo Morais <goncalo.p.morais@hotmail.com>
2025-05-29 20:15:24 +0100

    Simplify database environment variable handling in `EnvironmentApp`.

--- code/backend/LiftDropApi/src/main/kotlin/pt/isel/liftdrop/EnvironmentApp.kt
+++ code/backend/LiftDropApi/src/main/kotlin/pt/isel/liftdrop/EnvironmentApp.kt
@@ -2,18 +2,15 @@ package pt.isel.liftdrop
 
 object EnvironmentApp {
     fun getDbUrl(): String {
-        val dbUrl = System.getenv("DB_URL") ?: return "jdbc:postgresql://localhost:5432/liftdrop"
-        return "jdbc:${dbUrl}"
+        return System.getenv("DB_URL") ?: return "jdbc:postgresql://localhost:5432/liftdrop"
     }
 
     fun getDbUser(): String {
-        val dbUrl = System.getenv("DB_URL") ?: return "postgres"
-        return dbUrl.split("://")[1].split(":")[0]
+        return  System.getenv("DB_USER") ?: return "postgres"
     }
 
     fun getDbPassword(): String {
-        val dbUrl = System.getenv("DB_URL") ?: return "postgres"
-        return dbUrl.split(":")[2].split("@")[0]
+        return System.getenv("DB_PASS") ?: return "postgres"
     }
 
 //    postgresql://liftdrop_ceib_user:NjEbzbVyGBsvF0AoG7c8C4VOp5u3OblK@dpg-d0s9dkjuibrs738179c0-a/liftdrop_ceib
