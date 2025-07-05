c779305eccb14e49f66d29326ed11b2aba79252a
49424 - Joao Ramos <a49424@alunos.isel.pt>
2025-07-05 21:17:24 +0100

    *UPDATED* Result messages

--- code/backend/LiftDropDomain/src/main/kotlin/pt/isel/liftdrop/ResultMessage.kt
+++ code/backend/LiftDropDomain/src/main/kotlin/pt/isel/liftdrop/ResultMessage.kt
@@ -11,32 +11,32 @@ data class ResultMessage(
             ResultMessage(
                 type = ResultType.SUCCESS,
                 subType = ResultSubType.ACCEPT,
-                message = "Delivery request accepted successfully.",
-                detail = "The request has been accepted by the courier.",
+                message = "Pedido de entrega aceite com sucesso.",
+                detail = "O pedido foi aceite pelo estafeta com sucesso.",
             )
 
         fun declineSuccessMessage(): ResultMessage =
             ResultMessage(
                 type = ResultType.SUCCESS,
                 subType = ResultSubType.DECLINE,
-                message = "Delivery request declined successfully.",
-                detail = "The request has been declined by the courier.",
+                message = "Pedido de entrega recusado com sucesso.",
+                detail = "O pedido foi recusado pelo estafeta com sucesso.",
             )
 
         fun acceptErrorMessage(): ResultMessage =
             ResultMessage(
                 type = ResultType.ERROR,
                 subType = ResultSubType.ACCEPT,
-                message = "Failed to accept the delivery request.",
-                detail = "The request could not be accepted by the courier.",
+                message = "Ocorreu um erro ao aceitar o pedido de entrega.",
+                detail = "O pedido não pôde ser aceite pelo estafeta.",
             )
 
         fun declineErrorMessage(): ResultMessage =
             ResultMessage(
                 type = ResultType.ERROR,
                 subType = ResultSubType.DECLINE,
-                message = "Failed to decline the delivery request.",
-                detail = "The request could not be declined by the courier.",
+                message = "Ocorreu um erro ao recusar o pedido de entrega.",
+                detail = "O pedido não pôde ser recusado pelo estafeta.",
             )
     }
 }
